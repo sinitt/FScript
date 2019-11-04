2cf69c8ca8051defbd6d99dffc56921422426079
sinitt <51162096+sinitt@users.noreply.github.com>
2019-11-04 20:55:18 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import sys, os, time
+os.system("apt update")
+os.system("apt-get install hydra")
+os.system("apt-get install nmap")
+os.system("pkg install hydra")
+os.system("pkg install nmap")
